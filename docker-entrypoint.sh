e705a0af3f0cda2336ee79517e2be91487c33059
leodanis <leodanismiranda@gmail.com>
2025-05-22 08:35:22 -0400

    nuevos

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env sh
 set -e
 
-# 1) Sustituir ${PORT} en la plantilla y generar default.conf
+echo ">>> Entrypoint: sustituyendo \${PORT} en nginx.conf.template"
+
+# Si PORT no está definida, nombra un valor por defecto (80)
+: "${PORT:=80}"
+
+# Reemplaza ${PORT} en la plantilla y genera default.conf
 envsubst '$PORT' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
-# 2) Ejecutar el comando que se pasa al contenedor (CMD)
+echo ">>> Entrypoint: finished envsubst, PORT=${PORT}"
+echo ">>> Contenido de /etc/nginx/conf.d/default.conf:"
+cat /etc/nginx/conf.d/default.conf
+
+# Ejecuta el comando que llegó (CMD)
 exec "$@"
+
